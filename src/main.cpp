e1634b5dde2292c63635c9674ce6cb6d391675cb
TRocket <TRocket@Itchypp.github>
2012-12-03 20:33:00 +0000

    added first static function say()

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
+#include "static.cpp"
 
 using namespace std;
 
 int main(){
-	cout << "Hello world IPPX!";
+	cout << "Hello world IPPX!" << endl;
+
+	std::string* arg1;
+	arg1 = new std::string("Hello World");
+	cout << &arg1 << endl;
+	ippx::stat::say(arg1);
 }
 
 
---
+++ src/static.cpp
@@ -0,0 +1,17 @@
+/*
+ * This file contains all the static functions to execute blocks
+ */
+#include "iostream";
+
+
+namespace ippx {
+
+namespace stat { //NS: ippx:stat
+
+static void say(std::string* s) {
+	std::cout << *s;
+}
+
+};
+
+};
