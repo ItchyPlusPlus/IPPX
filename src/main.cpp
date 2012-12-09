bd389afed0e83424f7e1136bb7d72040508eb1ea
TRocket <TRocket@Itchypp.github>
2012-12-09 13:47:32 +0000

    forgot some other changes to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,17 @@
 #include <iostream>
-#include "static.cpp"
+#include "func.cpp"
 
 using namespace std;
 
-/**
- * this is a test
- */
+
 int main(){
 	cout << "Hello world IPPX!" << endl;
 
 	std::string* arg1;
 	arg1 = new std::string("Hello World");
 	cout << &arg1 << endl;
-	cout << &ippx::stat::say << endl;
-	cout << &ippx::stat::shout << endl;
-	ippx::stat::say(arg1);
+	cout << &ippx::func::say << endl;
+	ippx::func::say(arg1);
 }
 
 
